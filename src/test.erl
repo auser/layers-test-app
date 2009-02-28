87e4cbda739f1a96c7d98504224e9ad0fc344881
Ari Lerner <alerner@YPCMC05591.local>
2009-02-27 17:06:19 -0800

    Updated stack test app

--- src/test.erl
+++ src/test.erl
@@ -1,26 +1,32 @@
 -module (test).
 
 %% application callbacks
--export([start/0, layers_receive/1]).
+-export([start/2, start/0, layers_receive/1]).
 -export ([test/0]).
 
 -define (MAXIMUM_RESTARTS, 10).
 -define (MAX_DELAY_TIME, 60).
 
 test() ->
-  converse:send("0.0.0.0", {data, "hey you guys"}).
-  
+  converse:send_message("0.0.0.0", {lookup, whisper:encrypt("a")}).
+
+start(_Type, Config) ->
+	io:format("Starting ~p with ~p~n", [?MODULE, Config]).
+
+
 start() ->    
 	layers:init(),
 	layers:add(converse, [{port, 22002}]),
 	layers:add(whisper, []),
 	layers:add(where, []),
-	layers:add(layers_test_app, []),
+	layers:add(?MODULE, []),
 	layers:start().
 
 layers_receive(Msg) ->
   case Msg of
     {data, Socket, Data} ->
       io:format("Unencrypted in ~p data: ~p~n", [?MODULE, Data]),
-      converse:reply(Socket, "Thanks!")
+      converse:reply(Socket, "Thanks!");
+    Else -> 
+      io:format("Received ~p in ~p~n", [Else, ?MODULE])
   end.

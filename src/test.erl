ccf90eff47d5994e1f1fac5a080e7b2f8c558e44
Ari Lerner <alerner@YPCMC05591.local>
2009-03-03 00:51:44 -0800

    added consistancy to the test receive

--- src/test.erl
+++ src/test.erl
@@ -4,11 +4,12 @@
 -export([start/2, start/0, layers_receive/1]).
 -export ([test/0]).
 
+-define (PORT, 22002).
 -define (MAXIMUM_RESTARTS, 10).
 -define (MAX_DELAY_TIME, 60).
 
 test() ->
-  converse:send_message("0.0.0.0", {lookup, whisper:encrypt("a")}).
+  converse:send_message("0.0.0.0", ?PORT, {lookup, whisper:encrypt("a")}).
 
 start(_Type, Config) ->
 	io:format("Starting ~p with ~p~n", [?MODULE, Config]).
@@ -16,7 +17,7 @@ start(_Type, Config) ->
 
 start() ->    
 	layers:init(),
-	layers:add(converse, [{port, 22002}]),
+	layers:add(converse, [{port, ?PORT}]),
 	layers:add(whisper, []),
 	layers:add(where, []),
 	layers:add(?MODULE, []),
